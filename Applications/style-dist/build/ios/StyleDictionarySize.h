e5b687eade709d02ba061728c51f82391e4a1434
Vishal Tadha <tadhavishal@gmail.com>
2019-06-15 12:32:14 +0530

    Updated style base variables

--- Applications/style-dist/build/ios/StyleDictionarySize.h
+++ Applications/style-dist/build/ios/StyleDictionarySize.h
@@ -2,7 +2,7 @@
 // StyleDictionarySize.h
 //
 // Do not edit directly
-// Generated on Sat, 15 Jun 2019 06:45:56 GMT
+// Generated on Sat, 15 Jun 2019 07:01:39 GMT
 //
 
 #import <Foundation/Foundation.h>
